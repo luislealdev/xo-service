bcf0f16d6b2d4ea04dd9571bf24eb8a30d55f400
Luis Leal <luisrrleal@gmail.com>
2024-11-25 22:52:48 -0500

    Remove role decorator

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -4,13 +4,13 @@ import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 import { Public } from 'src/decorator/public.decorator';
 import { Roles } from 'src/decorator/role.decorator';
-import { Role } from 'src/enum/role.enum';
+// import { Role } from 'src/enum/role.enum';
 
 @Controller('event')
 export class EventController {
   constructor(private readonly eventService: EventService) {}
 
-  @Roles(Role.ADMIN, Role.CREATOR)
+  // @Roles(Role.ADMIN, Role.CREATOR)
   @Post()
   create(@Body() createEventDto: CreateEventDto) {
     return this.eventService.create(createEventDto);
@@ -28,13 +28,13 @@ export class EventController {
     return this.eventService.findOne(id);
   }
 
-  @Roles(Role.ADMIN, Role.CREATOR)
+  // @Roles(Role.ADMIN, Role.CREATOR)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
     return this.eventService.update(id, updateEventDto);
   }
 
-  @Roles(Role.ADMIN, Role.CREATOR)
+  // @Roles(Role.ADMIN, Role.CREATOR)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.eventService.remove(id);
